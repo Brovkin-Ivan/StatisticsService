43469dcff07582dbfab05d6b2763f843f0b2333b
Brovkin Ivan <isbrovkinn@gmail.com>
2023-03-21 15:23:28 +0300

    Add extra tests and set jococo

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -5,11 +5,23 @@ import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
     @Test
-    void findMax() {
+    void findMaxWhenMaxFirst() {
         StatisticsServiceService service = new StatisticsServiceService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenMaxMiddle() {
+        StatisticsServiceService service = new StatisticsServiceService();
+
+        long[] incomesInBillions = {12, 5, 8, 40, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 40;
 
         long actual = service.findMax(incomesInBillions);
 
